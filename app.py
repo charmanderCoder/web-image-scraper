44c61a21b10bb0b9e99b24083452975ad7968263
Prajwal Jain <prajwal@appbrew.tech>
2025-03-03 12:21:43 +0530

    feat: working demo for scraping

--- app.py
+++ app.py
@@ -119,4 +119,4 @@ def download_zip(folder_name):
         return jsonify({'error': 'Failed to create zip file', 'status': 'error'}), 500, {'Content-Type': 'application/json'}
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
